739f42d2c450063e63b3386e89f20fa0a25a8b32
LiaoZhengsong <pkujereok@gmail.com>
2019-05-20 06:47:20 -0700

    20190520

---
+++ 20190520/String.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+class String {
+public:
+    String();
+    String(const char *);
+    String(const String&);
+    ~String();
+    void print()
+    {
+        std::cout << _pstr << std::endl;
+    }
+    String &operator=(const String &);
+    String &operator=(const char *);
+
+    String &operator+=(const String &);
+    String &operator+=(const char *);
+
+    char &operator[](std::size_t index);
+    const char &operator[](std::size_t index) const;
+
+    std::size_t size() const;
+    const char* c_str() const;
+
+    friend bool operator==(const String &, const String &);
+    friend bool operator!=(const String &, const String &);
+
+    friend bool operator<(const String &, const String &);
+    friend bool operator>(const String &, const String &);
+    friend bool operator<=(const String &, const String &);
+    friend bool operator>=(const String &, const String &);
+
+    friend std::ostream &operator<<(std::ostream &os, const String &s);
+    friend std::istream &operator>>(std::istream &is, String &s);
+
+private:
+    char * _pstr;
+
+};
+
+String operator+(const String &, const String &);
+String operator+(const String &, const char *);
+String operator+(const char *, const String &);
+
